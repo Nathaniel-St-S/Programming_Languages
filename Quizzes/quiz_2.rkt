4d415ce7808cde35ee726b9c7b48a922fd758215
NathanielS. <savoury.nathaniel@gmail.com>
2025-08-30 03:33:14 -0400

    added setup for quiz 2 and hw

---
+++ Quizzes/quiz_2.rkt
@@ -0,0 +1,14 @@
+#lang eopl
+(require rackunit "../Extras/Marco_Provided_Code/eopl-extras.rkt")
+
+;; Question 2.12 in the book
+
+#|
+Exercise 2.12: Implement the stack data type of exercise 2.4 using a procedural
+representation.
+
+Exercise 2.4: Consider the data type of stacks of values, with an interface consist-
+ing of the procedures empty-stack, push, pop, top, and empty-stack?. Write a
+specification for these operations in the style of the example above. Which operations
+are constructors and which are observers?
+|#
